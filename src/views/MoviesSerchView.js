c6e889548ba7e19094b73557531e16e55c2b7b84
taraiihor <63466237+taraiihor@users.noreply.github.com>
2021-01-09 21:06:23 +0200

    Пагінація при пошуку фільмів

--- src/views/MoviesSerchView.js
+++ src/views/MoviesSerchView.js
@@ -3,22 +3,29 @@ import { useRouteMatch, Link, useHistory, useLocation } from 'react-router-dom';
 import * as moviesApi from '../service/api';
 import ErrorView from './ErrorView';
 
+import { Pagination } from '@material-ui/lab';
+import useStyles from '../service/PaginationStyles';
+
 export default function MoviesSerchView() {
   const { url } = useRouteMatch();
   const [movie, setMovie] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
+  const [totalPage, setTotalPage] = useState(0);
+  //Запуск стилів пагінації
+  const classes = useStyles();
 
   const history = useHistory();
   // console.log(history);
   const location = useLocation();
   // console.log(location);
   const searchMovie = new URLSearchParams(location.search).get('query') ?? '';
+  const page = new URLSearchParams(location.search).get('page') ?? 1;
 
-  // console.log(searchMovie);
-  const updatePage = () => {
-    setPage(page + 1);
+  //фунція переключання пагінації і збереження сорінки
+  const onPaginationPage = (event, page) => {
+    history.push({ ...location, search: `query=${searchMovie}&page=${page}` });
   };
   const handleNameChangle = event => {
     setSearchQuery(event.target.value);
@@ -33,19 +40,21 @@ export default function MoviesSerchView() {
     setMovie(null);
     setError(null);
   };
+
   useEffect(() => {
     if (searchMovie === '') {
       return;
     }
     moviesApi
       .fetchSearchMovies(searchMovie, page)
-      .then(({ results }) => {
+      .then(({ results, total_pages }) => {
         if (results.length === 0) {
           return Promise.reject(
             new Error(`По вашому пошуку нічого не знайшли`),
           );
         }
         setMovie(results);
+        setTotalPage(total_pages);
       })
       .catch(error => {
         setError(error);
@@ -73,10 +82,14 @@ export default function MoviesSerchView() {
           ))}
         </ul>
       )}
-      {movie && (
-        <button type="button" onClick={updatePage}>
-          наступні фільми
-        </button>
+
+      {totalPage > 1 && (
+        <Pagination
+          className={classes.root}
+          count={totalPage}
+          onChange={onPaginationPage}
+          page={Number(page)}
+        />
       )}
     </>
   );
